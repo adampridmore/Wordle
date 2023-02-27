69583f4a45b67f01f944148008fc11252f872ff9
David Betteridge <davidabetteridge@gmail.com>
2023-02-27 09:39:39 +0000

    WIP

--- David_CSharp/Program.cs
+++ David_CSharp/Program.cs
@@ -6,18 +6,32 @@
   {
     try
     {
-      await api.DownloadWords("words.txt");
-      Console.WriteLine("Downloaded word list");
+      if (!File.Exists("words.txt"))
+      {
+        await api.DownloadWords("words.txt");
+        Console.WriteLine("Downloaded word list");
+      }
+
+      var words = await File.ReadAllLinesAsync("words.txt");
 
       var teamId = await api.RegisterTeam("Davids Example");
       Console.Write(teamId);
-      
+
       var gameId = await api.StartNewGame(teamId);
       Console.Write(gameId);
 
-      var guess = await api.GuessWord(gameId, "ABOUT");
-      Console.WriteLine(guess.Score);
-      Console.WriteLine(guess.State.ToString());
+      // Pick a word at random
+      var rnd = new Random();
+      while (true)
+      {
+        var wordToTry = words[rnd.NextInt64(words.LongCount())];
+
+        var guess = await api.GuessWord(gameId, wordToTry);
+        Console.WriteLine(guess.Score);
+        Console.WriteLine(guess.State.ToString());
+
+        words = filterUsingScore(words, wordToTry, guess.Score);
+      }
 
       var game = await api.GetGame(gameId);
       Console.WriteLine(game.State.ToString());
@@ -27,5 +41,24 @@
       Console.WriteLine("\nException Caught!");
       Console.WriteLine("Message :{0} ", e.Message);
     }
+  }
+
+  private static string[] filterUsingScore(string[] words, string guess, string score)
+  {
+    return words.Where(word => IsValid(word, guess, score)).ToArray();
+  }
+
+  private static bool IsValid(string word, string guess, string score)
+  {
+    for (int i = 0; i < word.Length; i++)
+    {
+      if (score[i] == 'G' && word[i] != guess[i])
+        return false;
+
+      if (score[i] == 'Y' && !word.Contains(guess[i]))
+        return false;
+    }
+    return true;
+
   }
 }
