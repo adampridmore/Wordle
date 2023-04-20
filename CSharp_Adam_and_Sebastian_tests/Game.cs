4ca79ca92c8bfaef303c91c07102ddc34c1976b6
Adam Pridmore <adam_pridomre@hotmmail.com>
2023-04-20 21:31:04 +0100

    Add basic game and tests

---
+++ CSharp_Adam_and_Sebastian_tests/Game.cs
@@ -0,0 +1,39 @@
+namespace CSharp_Adam_and_Sebastian_tests;
+
+public enum GameState
+{
+  InProgress,
+  Won,
+  Lost
+}
+
+public class Game {
+
+  private const string WinScore = "GGGGG";
+  public GameState State { get; }
+
+  public string LastGuessScore { get; }
+
+  public int GuessCount { get; }
+
+  private readonly string _word;
+
+  public static Game NewGame(string word){
+    return new Game(word, 0, "", GameState.Won);
+  }
+
+  private Game(string word, int guessCount, string lastGuessScore, GameState state){
+    _word = word;
+    GuessCount = guessCount;
+    LastGuessScore = lastGuessScore;
+    State = state;
+  }
+
+  public Game MakeGuess(string guessWord){    
+    var lastGuessScore = Solver.ScoreGuessAgainstWord(guessWord, _word);
+
+    var nextGameState = lastGuessScore == WinScore?GameState.Won:GameState.InProgress;
+
+    return new Game(_word, GuessCount+1, lastGuessScore, nextGameState);
+  }
+}
---
+++ CSharp_Adam_and_Sebastian_tests/GameTests.cs
@@ -0,0 +1,30 @@
+namespace CSharp_Adam_and_Sebastian_tests;
+
+public class GameTests
+{
+  [Test]
+  public void new_game_with_one_winning_guess()
+  {
+    var game = Game.NewGame("GREEN");
+
+    var nextGuessGame = game.MakeGuess("GREEN");
+    
+    Assert.That(nextGuessGame.State, Is.EqualTo(GameState.Won));
+    Assert.That(nextGuessGame.LastGuessScore, Is.EqualTo("GGGGG"));
+
+    Assert.That(nextGuessGame.GuessCount, Is.EqualTo(1));
+  }
+
+  [Test]
+  public void incorrect_guess()
+  {
+    var game = Game.NewGame("GREEN");
+
+    var nextGuessGame = game.MakeGuess("PAPER");
+    
+    Assert.That(nextGuessGame.State, Is.EqualTo(GameState.InProgress));
+    Assert.That(nextGuessGame.LastGuessScore, Is.EqualTo("   GY"));
+
+    Assert.That(nextGuessGame.GuessCount, Is.EqualTo(1));
+  }
+}
