df1a2a79ac3ec5b4fc0b498f51d5f69f69c461a2
Adam Pridmore <320922+adampridmore@users.noreply.github.com>
2023-05-06 12:10:34 +0100

    Play all games

---
+++ CSharp_Adam_and_Sebastian_runner/PlayAllGames.cs
@@ -0,0 +1,26 @@
+namespace Wordle;
+
+public class PlayAllGames{
+  public static void Run(){
+    var wordGuesser = new WordGuesser();
+
+    using (var writer = System.IO.File.CreateText("words-solve-count.csv")){
+      foreach(String word in wordGuesser.Words){
+        var stopWatch = System.Diagnostics.Stopwatch.StartNew();
+
+        System.Console.WriteLine($"Word to play: {word}");
+      
+        var game = Game.NewGame(word);
+
+        var player = new Player(game, wordGuesser);
+
+        var solution = player.SolveGame();
+
+        var message = $"Word: {word} guesses: {solution.Item2.GuessCount} {stopWatch.ElapsedMilliseconds}ms";
+        
+        System.Console.WriteLine(message);
+        writer.WriteLine(message);
+      }
+    }
+  }
+}
