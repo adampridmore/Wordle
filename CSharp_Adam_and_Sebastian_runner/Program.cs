b9d0b0b84b3483b7f43d7b23333a2c9bde6210ee
Adam Pridmore <320922+adampridmore@users.noreply.github.com>
2023-05-06 11:48:15 +0100

    Extract WebSolver

--- CSharp_Adam_and_Sebastian_runner/Program.cs
+++ CSharp_Adam_and_Sebastian_runner/Program.cs
@@ -4,52 +4,8 @@ using Wordle;
 
 public static class Program
 {
-  static readonly Api Api = new Api();
-
   static async Task Main()
   {
-    try
-    {
-      if (!File.Exists("words.txt"))
-      {
-        await Api.DownloadWords("words.txt");
-        Console.WriteLine("Downloaded word list");
-      }
-
-      // var teamId = await Api.RegisterTeam("Example");
-      // Console.WriteLine(teamId);
-
-      var teamId = new System.Guid("d24641c0-0fec-457b-b257-19344790aad9"); // SeeSharpers
-
-      var gameId = await Api.StartNewGame(teamId);
-      Console.WriteLine("GameId: " + gameId);
-
-      var firstGuess = "words";
-
-      var newGuessResponse = await  Api.GuessWord(gameId, firstGuess);
-      
-      Console.WriteLine("newGuessResponse: " + newGuessResponse);
-
-      var solver = new WordGuesser(File.ReadAllLines("words.txt").ToList());
-    
-      // TODO: Fix previousGuesses array
-      var nextGuess = solver.GetNextGuess(firstGuess, newGuessResponse.Score, new String[]{});
-      Console.WriteLine($"Score: {newGuessResponse.Score} NextGuess: {nextGuess}");
-      
-      while(newGuessResponse.State == GameState.InProgress){
-        
-        // TODO: Fix previousGuesses array
-        nextGuess = solver.GetNextGuess(nextGuess, newGuessResponse.Score, new String[]{});
-
-        newGuessResponse = await Api.GuessWord(gameId, nextGuess);
-
-        Console.WriteLine($"Score: {newGuessResponse.Score} NextGuess: {nextGuess} State: {newGuessResponse.State}");
-      } 
-    }
-    catch (Exception e)
-    {
-      Console.WriteLine("\nException Caught!");
-      Console.WriteLine("Message :{0} ", e.Message);
-    }
+    await WebSolver.Run();
   }
 }
---
+++ CSharp_Adam_and_Sebastian_runner/WebSolver.cs
@@ -0,0 +1,52 @@
+namespace Wordle;
+
+using Wordle.Models;
+
+public static class WebSolver{
+  static readonly Api Api = new Api();
+  public static async Task Run(){
+    try
+    {
+      if (!File.Exists("words.txt"))
+      {
+        await Api.DownloadWords("words.txt");
+        Console.WriteLine("Downloaded word list");
+      }
+
+      // var teamId = await Api.RegisterTeam("Example");
+      // Console.WriteLine(teamId);
+
+      var teamId = new System.Guid("d24641c0-0fec-457b-b257-19344790aad9"); // SeeSharpers
+
+      var gameId = await Api.StartNewGame(teamId);
+      Console.WriteLine("GameId: " + gameId);
+
+      var firstGuess = "words";
+
+      var newGuessResponse = await  Api.GuessWord(gameId, firstGuess);
+      
+      Console.WriteLine("newGuessResponse: " + newGuessResponse);
+
+      var solver = new WordGuesser(File.ReadAllLines("words.txt").ToList());
+    
+      // TODO: Fix previousGuesses array
+      var nextGuess = solver.GetNextGuess(firstGuess, newGuessResponse.Score, new String[]{});
+      Console.WriteLine($"Score: {newGuessResponse.Score} NextGuess: {nextGuess}");
+      
+      while(newGuessResponse.State == GameState.InProgress){
+        
+        // TODO: Fix previousGuesses array
+        nextGuess = solver.GetNextGuess(nextGuess, newGuessResponse.Score, new String[]{});
+
+        newGuessResponse = await Api.GuessWord(gameId, nextGuess);
+
+        Console.WriteLine($"Score: {newGuessResponse.Score} NextGuess: {nextGuess} State: {newGuessResponse.State}");
+      } 
+    }
+    catch (Exception e)
+    {
+      Console.WriteLine("\nException Caught!");
+      Console.WriteLine("Message :{0} ", e.Message);
+    }
+  }
+}
